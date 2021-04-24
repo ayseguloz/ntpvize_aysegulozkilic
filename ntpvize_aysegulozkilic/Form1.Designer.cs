7cedcef7b6ae56be60a7633ae581ed1a027f8a5f
Ayşegül Özkılıç <aysegulozkilic@outlook.com>
2021-04-24 14:07:27 +0300

    Form ekranı tasarımı yapıldı.

--- ntpvize_aysegulozkilic/Form1.Designer.cs
+++ ntpvize_aysegulozkilic/Form1.Designer.cs
@@ -29,13 +29,148 @@ namespace ntpvize_aysegulozkilic
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.labeltarih = new System.Windows.Forms.Label();
+            this.labellink = new System.Windows.Forms.Label();
+            this.labelaciklama = new System.Windows.Forms.Label();
+            this.labalbaslik = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.groupBox1.Controls.Add(this.labeltarih);
+            this.groupBox1.Controls.Add(this.labellink);
+            this.groupBox1.Controls.Add(this.labelaciklama);
+            this.groupBox1.Controls.Add(this.labalbaslik);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1116, 220);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "En son Haber";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 226);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1116, 297);
+            this.dataGridView1.TabIndex = 8;
+            // 
+            // labeltarih
+            // 
+            this.labeltarih.AutoSize = true;
+            this.labeltarih.Location = new System.Drawing.Point(57, 147);
+            this.labeltarih.Name = "labeltarih";
+            this.labeltarih.Size = new System.Drawing.Size(66, 17);
+            this.labeltarih.TabIndex = 7;
+            this.labeltarih.Text = "labeltarih";
+            // 
+            // labellink
+            // 
+            this.labellink.AutoSize = true;
+            this.labellink.Location = new System.Drawing.Point(57, 110);
+            this.labellink.Name = "labellink";
+            this.labellink.Size = new System.Drawing.Size(59, 17);
+            this.labellink.TabIndex = 6;
+            this.labellink.Text = "labellink";
+            // 
+            // labelaciklama
+            // 
+            this.labelaciklama.AutoSize = true;
+            this.labelaciklama.Location = new System.Drawing.Point(87, 70);
+            this.labelaciklama.Name = "labelaciklama";
+            this.labelaciklama.Size = new System.Drawing.Size(93, 17);
+            this.labelaciklama.TabIndex = 5;
+            this.labelaciklama.Text = "labelacıklama";
+            // 
+            // labalbaslik
+            // 
+            this.labalbaslik.AutoSize = true;
+            this.labalbaslik.Location = new System.Drawing.Point(68, 32);
+            this.labalbaslik.Name = "labalbaslik";
+            this.labalbaslik.Size = new System.Drawing.Size(74, 17);
+            this.labalbaslik.TabIndex = 4;
+            this.labalbaslik.Text = "labelbaslik";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 147);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Tarih:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 110);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Link:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Açıklama:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Başlık:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1118, 524);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Güncel Haberler";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label labeltarih;
+        private System.Windows.Forms.Label labellink;
+        private System.Windows.Forms.Label labelaciklama;
+        private System.Windows.Forms.Label labalbaslik;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- ntpvize_aysegulozkilic/Form1.cs
+++ ntpvize_aysegulozkilic/Form1.cs
@@ -4,8 +4,10 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.IO;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 using System.Windows.Forms;
 
 namespace ntpvize_aysegulozkilic
@@ -16,5 +18,7 @@ namespace ntpvize_aysegulozkilic
         {
             InitializeComponent();
         }
+
+        
     }
 }
